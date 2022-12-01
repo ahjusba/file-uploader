a8dfdd17e8ab7ccec87eb004bfe7eea838804ade
gmasshole <jussi.rislakki@gmail.com>
2022-12-01 13:57:36 +0200

    Add two models (Member, File) which represent a registered user and a single file uploaded by a that user

--- dashboard/core/models.py
+++ dashboard/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+  username = models.CharField(max_length=150)
+  organization = models.CharField(max_length=150)
+
+  def __str__(self):
+    return self.name
+
+class File(models.Model):
+  filename = models.CharField(max_length=150)
+  uploader = models.ForeignKey(Member, on_delete=models.CASCADE)
+  upload_time = models.DateTimeField()
+  downloads = models.IntegerField(default=0)
+
+  def __str__(self):
+    return {self.filename, self.uploader, self.upload_time}
